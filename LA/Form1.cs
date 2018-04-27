f5eaf06d5edb9abb7adc23e2833f83db6f5758e9
Astro <astro1247w@gmail.com>
2018-04-27 18:31:54 +0300

    Вывод U(m x m) матрицы от изначальной A(m x n)

--- LA/Form1.cs
+++ LA/Form1.cs
@@ -37,6 +37,8 @@ namespace LA
         private void DoSvd(object sender, EventArgs e)
         {
             MathNet.Numerics.LinearAlgebra.Factorization.Svd<double> svd = Matrix.GetSvd(Matrix.Parse(baseMatrixBox.Text));
+
+            //Заполняем координаты сингулярного вектора
             double[] singularVectorCoordinates = new double[3];
             int i = 0;
             foreach (var row in svd.S)
@@ -45,6 +47,24 @@ namespace LA
             }
             //singularVectorBox.Text = singularVectorCoordinates.ToString();
 
+            //Заполняем U матрицу
+            i = 0;
+            double[] values = new double[svd.U.ColumnCount*svd.U.RowCount];
+            string valuesStr = "";
+            foreach (var vals in svd.U.Storage.Enumerate())
+            {
+                values[i++] = double.Parse(vals.ToString());
+            }
+            i = 0;
+            Matrix Umatrix = Matrix.IdentityMatrix(svd.U.RowCount, svd.U.ColumnCount);
+            for (int j = 0; j < Umatrix.Columns; j++)
+            {
+                for (int k = 0; k < Umatrix.Rows; k++)
+                {
+                    Umatrix[j, k] = values[i++];
+                }
+            }
+            uMatrixBox.Text = Umatrix.ToString();
         }
     }
 }
